287ecd3d3f915988773a0e231d1034ef3357a78d
Fatih I <117350581+fatihh34@users.noreply.github.com>
2023-06-21 14:12:05 +0300

    21.06

--- src/test/java/AppiumBrowser.java
+++ src/test/java/AppiumBrowser.java
@@ -1,9 +1,12 @@
 
+import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.remote.AndroidMobileCapabilityType;
 import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
@@ -12,7 +15,7 @@ import java.net.URL;
 public class AppiumBrowser {
 
     @Test
-            public void Mobil() throws MalformedURLException {
+            public void Mobil() throws MalformedURLException, InterruptedException {
 
         DesiredCapabilities desiredCapabilities= new DesiredCapabilities();
         desiredCapabilities.setCapability("platformName","Android");
@@ -31,6 +34,15 @@ public class AppiumBrowser {
         //AndroidDriver<AndroidElement> driver = new AndroidDriver<AndroidElement>(new URL("http://localhost:4723"), caps);
 
 
-        driver.get("https://www.amazon.com/");
+        driver.get("https://www.amazon.com.tr/");
+        Thread.sleep(2000);
+        //driver.findElement(By.id("com.android.chrome:id/positive_button"));
+        //Thread.sleep(3000);
+        //MobileElement amazon = driver.findElement(By.xpath("//*[@class='nav-sprite nav-logo-base']"));
+        //Assert.assertTrue(amazon.isDisplayed());
+
+        MobileElement girisButon = driver.findElement(By.xpath("//*[@class='nav-a nav-show-sign-in']"));
+        girisButon.click();
+
     }
 }
